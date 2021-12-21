0b653a0824a15fc082d85d1019ef55a8ae33c353
samismail <samismail2010@gmail.com>
2021-12-20 22:13:46 -0600

    starting question function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,14 @@
-var scoreBoardEl = document.querySelector("#score");
-var timerEl = document.querySelector("#time");
+// header
+var scoreBoardEl = document.getElementById("score");
+var timerEl = document.getElementById("time");
+var startBtn = document.getElementById("start");
 
+
+// timer element
+var startTimer = function {
+    var timeLimit 
+
+}
 
 //quiz question element
 var question = [
@@ -9,14 +17,14 @@ var question = [
         answers: [
             {text: "Michael Jordan"},
             {text: "Charles Barkley"},
-            {text: "Magic Johnson"},
+            {text: "Magic Johnson", isCorrect: true},
         ]
     },
     {
         question: "60 point triple-double has been done 1 time in the history of the NBA, which on these players did it?",
         answers: [
             {text: "Lebron James"},
-            {text: "James Harden"},
+            {text: "James Harden", isCorrect: true},
             {text: "Wilt Chamberlein"},
         ]
     },
@@ -25,17 +33,24 @@ var question = [
         answers: [
             {text: "Pheonix Suns"},
             {text: "Charlotte Bobcats"},
-            {text: "Dallas Mavericks"},
+            {text: "Dallas Mavericks", isCorrect: true},
         ]
     },
     {
         question: "Lebron James won his 1st championship in 2012, which team did he defeat?",
         answers: [
             {text: "San Antonio Spurs"},
-            {text: "Oklahoma City Thunders"},
+            {text: "Oklahoma City Thunders", isCorrect: true},
             {text: "Dallas Mavericks"},
         ]
     },
 ];
 
+var score = 0;
+var qQuestion = 0;
+
+
 //questions and score function
+function questionAndScore() {
+    if
+}
