427a7836eedb27a80ddd98651a0e6216c4f10e1f
samismail <samismail2010@gmail.com>
2021-12-20 21:44:28 -0600

    add time element

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,41 @@
+var scoreBoardEl = document.querySelector("#score");
+var timerEl = document.querySelector("#time");
+
+
+//quiz question element
 var question = [
     {
-        question: "",
+        question: "In 1989, which NBA player was voted league MVP?",
         answers: [
-            {text:},
-            {text:},
-            {text:},
+            {text: "Michael Jordan"},
+            {text: "Charles Barkley"},
+            {text: "Magic Johnson"},
         ]
     },
     {
-        question: "",
+        question: "60 point triple-double has been done 1 time in the history of the NBA, which on these players did it?",
         answers: [
-            {text:},
-            {text:},
-            {text:},
+            {text: "Lebron James"},
+            {text: "James Harden"},
+            {text: "Wilt Chamberlein"},
         ]
     },
     {
-        question: "",
+        question: "Kobe Bryant scored 60 points in the 3 quarters against which of these teams?",
         answers: [
-            {text:},
-            {text:},
-            {text:},
+            {text: "Pheonix Suns"},
+            {text: "Charlotte Bobcats"},
+            {text: "Dallas Mavericks"},
         ]
     },
     {
-        question: "",
+        question: "Lebron James won his 1st championship in 2012, which team did he defeat?",
         answers: [
-            {text:},
-            {text:},
-            {text:},
+            {text: "San Antonio Spurs"},
+            {text: "Oklahoma City Thunders"},
+            {text: "Dallas Mavericks"},
         ]
     },
 ];
+
+//questions and score function
